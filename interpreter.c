472daaf03870fa6e5662c255662df47ecd93708f
amanijam <amani.jammoul@mail.mcgill.ca>
2022-03-01 12:36:17 -0500

    copy our a1 code

---
+++ interpreter.c
@@ -0,0 +1,162 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> 
+
+#include "shellmemory.h"
+#include "shell.h"
+
+int MAX_ARGS_SIZE = 7;
+
+int help();
+int quit();
+int badcommand();
+int badcommandSet();
+int set(char* var, char* value);
+int print(char* var);
+int run(char* script);
+int badcommandFileDoesNotExist();
+int ls();
+
+// Interpret commands and their arguments
+int interpreter(char* command_args[], int args_size){
+	int i;
+
+	if (args_size < 1) {
+		return badcommand();
+	}
+
+	for(i = 0; i < args_size; i++){ //strip spaces new line etc
+		command_args[i][strcspn(command_args[i], "\r\n")] = 0;
+	}
+
+	if (strcmp(command_args[0], "help")==0){
+	    if (args_size != 1) return badcommand();
+	    return help();
+	
+	} else if (strcmp(command_args[0], "quit")==0) {
+		if (args_size != 1) return badcommand();
+		return quit();
+
+	} else if (strcmp(command_args[0], "set")==0) {
+		if(args_size > MAX_ARGS_SIZE) {
+			return badcommandSet();
+		}
+		else if (args_size < 3) {
+			return badcommand();
+		}
+		else {
+			char *link = " ";
+			char buffer[1000];
+			strcpy(buffer, command_args[2]);
+			for (int i = 3; i < args_size; i++){
+				strcat(buffer, link);
+				strcat(buffer, command_args[i]);
+			}
+
+			return set(command_args[1], buffer);
+		}
+		
+	} else if (strcmp(command_args[0], "echo") == 0) {
+		if (args_size != 2) return badcommand();
+		char buffer[100];
+		strcpy(buffer, command_args[1]);
+		if(buffer[0] == '$'){
+			memmove(buffer, buffer+1, strlen(buffer));
+			return print(buffer);
+		}
+		else{
+			printf("%s\n", buffer);
+		}
+
+	} else if (strcmp(command_args[0], "print")==0) {
+		if (args_size != 2) return badcommand();
+		return print(command_args[1]);
+	
+	} else if (strcmp(command_args[0], "run")==0) {
+		if (args_size != 2) return badcommand();
+		return run(command_args[1]);
+	
+	} else if (strcmp(command_args[0], "my_ls")==0) {
+		if (args_size != 1) return badcommand();
+		return ls();
+
+	} else return badcommand();
+}
+
+int help(){
+
+	char help_string[] = "COMMAND			DESCRIPTION\n \
+		help			Displays all the commands\n \
+		quit			Exits / terminates the shell with “Bye!”\n \
+		set VAR STRING		Assigns a value to shell memory\n \
+		print VAR		Displays the STRING assigned to VAR\n \
+		echo VAR STRING 	Displays the string on a new line \n \
+		run SCRIPT.TXT		Executes the file SCRIPT.TXT\n \
+		my_ls 			Lists all files present in current directory\n \
+		(multiple commands)	Runs up to 5 commands per line, separated by\n \
+					 semicolons (;) added to the end of the last word\n \
+					 in each command";
+	printf("%s\n", help_string);
+	return 0;
+}
+
+int quit(){
+	printf("%s\n", "Bye!");
+	exit(0);
+}
+
+int badcommand(){
+	printf("%s\n", "Unknown Command");
+	return 1;
+}
+
+int badcommandSet(){
+	printf("%s\n", "Bad command: Too many tokens");
+	return 2;
+}
+
+// For run command only
+int badcommandFileDoesNotExist(){
+	printf("%s\n", "Bad command: File not found");
+	return 3;
+}
+
+int set(char* var, char* value){
+	mem_set_value(var, value);
+	return 0;
+}
+
+int print(char* var){
+	printf("%s\n", mem_get_value(var)); 
+	return 0;
+}
+
+int run(char* script){
+	int errCode = 0;
+	char line[1000];
+	FILE *p = fopen(script,"rt");  // the program is in a file
+
+	if(p == NULL){
+		return badcommandFileDoesNotExist();
+	}
+
+	fgets(line, 999, p);
+	while(1){
+		errCode = parseInput(line);	// which calls interpreter()
+		memset(line, 0, sizeof(line));
+
+		if(feof(p)){
+			break;
+		}
+		fgets(line, 999, p);
+
+	}
+
+    fclose(p);
+
+	return errCode;
+}
+
+int ls(){
+	return system("ls -1"); // lists directories in alphabetical order, 1 entry per line
+}
